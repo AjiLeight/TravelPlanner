bbe57aefe7996e56d2c1d0f3c76c2543db4c32e9
suhaibvazin <suhaibvazin321@gmail.com>
2023-09-17 18:20:15 +0530

    MongoDB integration completed

--- src/main/java/com/personal/TravelPlanner/TravelPlannerApplication.java
+++ src/main/java/com/personal/TravelPlanner/TravelPlannerApplication.java
@@ -1,13 +1,32 @@
 package com.personal.TravelPlanner;
 
+import com.personal.TravelPlanner.entity.User;
+import com.personal.TravelPlanner.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@RequiredArgsConstructor
 @SpringBootApplication
-public class TravelPlannerApplication {
+public class TravelPlannerApplication implements CommandLineRunner {
 
+	private final UserRepository repository;
 	public static void main(String[] args) {
 		SpringApplication.run(TravelPlannerApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+//		repository.save(new User(
+//				"1",
+//				"suhaib",
+//				"vazin",
+//				"suhaibvazin321@gmail.com",
+//				"1234",
+//				"user"
+//		));
+		User user =repository.findByEmail("suhaibvazin321@gmail.com").orElseThrow();
+		System.out.println("Email is --------------------"+user.getEmail());
+	}
 }
